3af6586af1948eb8b62d7f0cd5ec9838fa2b389d
naught101 <naught101@naught101.org>
2017-02-10 15:33:58 +1100

    move test data to subdir

--- scripts/TestSingleSiteExperiment.R
+++ scripts/TestSingleSiteExperiment.R
@@ -1,9 +1,9 @@
 library("RJSONIO")
-inputFile <- "/Users/gab/Documents/admin/PALS/testing/Danny/TestInput_SingleSite.json"
+inputFile <- "data/TestInput_SingleSite.json"
 input <- fromJSON(paste(readLines(inputFile), collapse=""));
 Rruntime = system.time(source("SingleSiteExperiment.R"))
 print(paste('Time to run:',Rruntime[3]))
 output <- toJSON(output)
-fileConn<-file("output_SingleSite.json")
+fileConn<-file("data/output_SingleSite.json")
 writeLines(output, fileConn)
 close(fileConn)
